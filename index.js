c4f0092cf416861dbe4f9302e573d3d30969b035
Manohar R <manohar2004gr@gmail.com>
2024-10-30 00:07:01 +0530

    with msg21

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/",async(req, res)=>{
-    let data = await fetch("https://jsonplaceholder.typicode.com/todos/");
-    let response = await data.json();
-    res.json(response)
-    console.log("helloo")
-})
-app.get("/manu",(req,res)=>{
-    res.json({message:"its working"})
-})
+app.get("/", async (req, res) => {
+  let data = await fetch("https://jsonplaceholder.typicode.com/todos/");
+  let response = await data.json();
+  res.json(response);
+});
 
-app.listen(port,()=>{
-    console.log("successfully running!!")
-})
+app.get("/manu", (req, res) => {
+  res.json({ message: "It's working" });
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "API route is working" });
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port", port);
+});
