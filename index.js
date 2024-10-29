351dfa397cc7cee853446eb59ace85b462cd6b81
Manohar R <manohar2004gr@gmail.com>
2024-10-29 23:46:59 +0530

    new

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use("/",async(req, res)=>{
+app.use("/api",async(req, res)=>{
     let data = await fetch("https://jsonplaceholder.typicode.com/todos/");
     let response = await data.json();
     res.json(response)
